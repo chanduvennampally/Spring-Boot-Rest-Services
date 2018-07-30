0d52b972b16f29523751933174f9c6522b1e4b45
Aakash N <aakashnunna@gmail.com>
2018-07-29 18:08:50 -0700

    Updated Response for get USER

--- src/main/java/com/springboot/controller/UserController.java
+++ src/main/java/com/springboot/controller/UserController.java
@@ -3,11 +3,14 @@ package com.springboot.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springboot.customexception.ResourceNotFound;
 import com.springboot.model.User;
 import com.springboot.service.UserService;
 
@@ -23,7 +26,14 @@ public class UserController {
 	// get single user by id
 
 	@GetMapping("/user/{id}")
-	public User getUserById(@PathVariable(value = "id") Long id) {
-		return userService.findById(id);
+	public ResponseEntity<User> getUserById(@PathVariable(value = "id") Long id) {
+		
+		try {
+			User user = userService.findById(id);
+			return new ResponseEntity<User>(user, HttpStatus.OK);
+		} catch (ResourceNotFound e) {
+			return new ResponseEntity<User>( HttpStatus.NOT_FOUND);
+		}
+		
 	}
 }
